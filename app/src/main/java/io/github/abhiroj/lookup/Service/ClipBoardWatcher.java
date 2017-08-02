277bbb7e7e13296ddd001926908a048add43344a
abhiroj <abhiroj95@gmail.com>
2017-08-03 03:16:39 +0530

    feat: float view is on

--- app/src/main/java/io/github/abhiroj/lookup/Service/ClipBoardWatcher.java
+++ app/src/main/java/io/github/abhiroj/lookup/Service/ClipBoardWatcher.java
@@ -5,14 +5,25 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import io.github.abhiroj.lookup.R;
 
 import static io.github.abhiroj.lookup.Utils.Utility.isWord;
 
 public class ClipBoardWatcher extends Service {
+
+    private WindowManager windowManager;
+    private View view;
 
     public static String LOG_TAG=ClipBoardWatcher.class.getSimpleName();
 
@@ -35,6 +46,71 @@ public class ClipBoardWatcher extends Service {
             if(isWord(text))
             {
                Log.d(LOG_TAG,text);
+                Log.d(LOG_TAG,"Ready for the overlay!");
+
+
+                view= LayoutInflater.from(this).inflate(R.layout.floatview,null);
+                //Add the view to the window.
+                final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                        WindowManager.LayoutParams.WRAP_CONTENT,
+                        WindowManager.LayoutParams.WRAP_CONTENT,
+                        0,
+                        100,
+                        WindowManager.LayoutParams.TYPE_PHONE,
+                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                        PixelFormat.TRANSLUCENT);
+
+                params.gravity= Gravity.TOP | Gravity.LEFT;
+
+                windowManager=(WindowManager) getSystemService(WINDOW_SERVICE);
+                windowManager.addView(view,params);
+
+                view.setOnTouchListener(new View.OnTouchListener() {
+
+                    int lastAction;
+                    int initX;
+                    int initY;
+                    float initTouchX;
+                    float initTouchY;
+
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+
+                        switch(event.getAction())
+                        {
+                            case MotionEvent.ACTION_DOWN:
+
+                                initX=params.x;
+                                initY=params.y;
+
+                                initTouchX= event.getRawX();
+                                initTouchY=event.getRawY();
+
+                                lastAction=MotionEvent.ACTION_DOWN;
+                            return true;
+                            case MotionEvent.ACTION_UP:
+                                if(lastAction==MotionEvent.ACTION_DOWN)
+                                {
+                                    // TODO: Show the meaning of the word
+                                    Log.d(LOG_TAG,"Here we need to show the meaning!");
+                                }
+                                return true;
+                            case MotionEvent.ACTION_MOVE:
+                                params.x = initX + (int) (event.getRawX() - initTouchX);
+                                params.y = initY + (int) (event.getRawY() - initTouchY);
+                                Log.d(LOG_TAG,"Update X "+params.x+" Update Y "+params.y);
+
+                                windowManager.updateViewLayout(view,params);
+                                lastAction=MotionEvent.ACTION_MOVE;
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+
+                // TODO: API Call
+
             }
         }
         }
